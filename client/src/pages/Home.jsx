73a3b1a1afe134806702315730f2590c64b00591
Thatikonda Vikram <146024751+Vikram-0-7@users.noreply.github.com>
2026-07-22 23:02:28 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
 import Textimonials from '../components/Textimonials'
-import GenerateBtn from '../components/Generatebtn'
+import GenerateBtn from '../components/GenerateBtn'
 
 const Home = () => {
   return (
