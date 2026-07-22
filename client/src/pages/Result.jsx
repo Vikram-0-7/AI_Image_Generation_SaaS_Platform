7de7106af96b57d3bf41fda43b76e946138ed451
Thatikonda Vikram <146024751+Vikram-0-7@users.noreply.github.com>
2026-07-22 23:05:42 +0530

    Update Result.jsx

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 
 import { assets } from '../assets/assets'
-import { AppContext } from '../context/Appcontext'
+import { AppContext } from '../context/AppContext'
 const Result = () => {
   const { generateImage } = useContext(AppContext)
   const [isImageLoaded, setIsImageLoaded] = useState(false)
